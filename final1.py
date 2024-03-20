b360220f05c9bbbddb140ff37238d3ee49aa34a3
Tahmuras Pirimov <96793756+tahmurasp@users.noreply.github.com>
2024-03-20 19:15:28 -0400

    Create final1.py
    I upload both versions together

---
+++ final1.py
@@ -0,0 +1,238 @@
+# pip install streamlit langchain lanchain-openai beautifulsoup4 python-dotenv chromadb
+
+import streamlit as st
+
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import HuggingFaceHub
+
+
+load_dotenv()
+
+
+##--------------------------------------------------------
+
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(message.content)
+        else:
+            st.write(message.content)
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with MonarchAI", page_icon="🤖")
+    st.title("Methodist University Chatbot")
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+##---------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def get_vectorstore_from_url(url):
+    # get the text in document form
+    loader = WebBaseLoader(url)
+    document = loader.load()
+    
+    # split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+    
+    # create a vectorstore from the chunks
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+
+    return vector_store
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI()
+    
+    retriever = vector_store.as_retriever()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    
+    return retriever_chain
+    
+def get_conversational_rag_chain(retriever_chain): 
+    
+    llm = ChatOpenAI()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions only based on the below context:\n\n{context}\n\n\n"),
+      ("system", "Ignor any quories not written in the context"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+def get_response(user_input):
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_query
+    })
+    
+    return response['answer']
+
+
+
+# no sidebar
+website_url = [
+    "https://www.methodist.edu/",
+    "https://www.methodist.edu/explore-mu/numbers/",
+    "https://www.methodist.edu/academics/academic-calendar/campus-undergraduate-programs/",
+    "https://www.methodist.edu/academics/academic-calendar/final-exam-schedule/",
+    "https://www.methodist.edu/maps-directions/",
+    "https://www.methodist.edu/events/",
+    "https://www.methodist.edu/life-at-mu/public-safety-security/parking/",
+    "https://www.methodist.edu/emergency/",
+    "https://www.methodist.edu/news/"
+]
+
+if website_url is None or website_url == "":
+    st.info("Please enter a website URL")
+
+else:
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am a bot. How can I help you?"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)    
+
+    # user input
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+        
+       
+
+    # conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
